49a42ca8c47fffaf40fc6f367b35e0a34658f0c4
George Lee <ghrlee@gmail.com>
2015-09-08 15:50:45 -0500

    create stocks model, db, controller

---
+++ db/migrate/20150908204700_create_stocks.rb
@@ -0,0 +1,10 @@
+class CreateStocks < ActiveRecord::Migration
+  def change
+    create_table :stocks do |t|
+      t.string :company
+      t.string :symbol
+
+      t.timestamps null: false
+    end
+  end
+end
